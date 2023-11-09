b75317119289e398319453c00151d90aa0a2fdda
Khoi Nguyen <khoi@nguyen.me.uk>
2023-11-09 15:02:49 +0100

    Preselect first item

--- neovim.nix
+++ neovim.nix
@@ -113,6 +113,10 @@ in
           local cmp_action = require('lsp-zero').cmp_action()
 
           cmp.setup({
+            preselect = 'item',
+            completion = {
+              completeopt = 'menu,menuone,noinsert'
+            },
             sources = {
               {name = 'luasnip'},
               {name = 'path'},
