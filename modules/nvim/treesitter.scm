998c30385a200b36dc9faf3e1ab2f9103a9aeb7b
Khoi Nguyen <khoi@nguyen.me.uk>
2024-01-03 05:48:37 +0100

    Support .if(solve)``

--- modules/nvim/treesitter.scm
+++ modules/nvim/treesitter.scm
@@ -9,9 +9,27 @@
    (#set! injection.language "python")))
 
 (call_expression
+ function: (call_expression
+   function: (member_expression
+    object: (identifier) @_name
+      (#eq? @_name "py")))
+ arguments: ((template_string) @injection.content
+   (#offset! @injection.content 0 1 0 -1)
+   (#set! injection.language "python")))
+
+(call_expression
  function: (member_expression
    object: (identifier) @_name
      (#eq? @_name "html"))
+ arguments: ((template_string) @injection.content
+   (#offset! @injection.content 0 1 0 -1)
+   (#set! injection.language "html")))
+
+(call_expression
+ function: (call_expression
+   function: (member_expression
+    object: (identifier) @_name
+      (#eq? @_name "html")))
  arguments: ((template_string) @injection.content
    (#offset! @injection.content 0 1 0 -1)
    (#set! injection.language "html")))
@@ -25,9 +43,27 @@
    (#set! injection.language "tsx")))
 
 (call_expression
+ function: (call_expression
+   function: (member_expression
+    object: (identifier) @_name
+      (#eq? @_name "js")))
+ arguments: ((template_string) @injection.content
+   (#offset! @injection.content 0 1 0 -1)
+   (#set! injection.language "tsx")))
+
+(call_expression
  function: (member_expression
    object: (identifier) @_name
      (#eq? @_name "ts"))
+ arguments: ((template_string) @injection.content
+   (#offset! @injection.content 0 1 0 -1)
+   (#set! injection.language "tsx")))
+
+(call_expression
+ function: (call_expression
+   function: (member_expression
+    object: (identifier) @_name
+      (#eq? @_name "ts")))
  arguments: ((template_string) @injection.content
    (#offset! @injection.content 0 1 0 -1)
    (#set! injection.language "tsx")))
@@ -41,6 +77,16 @@
    (#set! injection.language "tsx")))
 
 (call_expression
+ function: (call_expression
+   function: (member_expression
+    object: (identifier) @_name
+      (#eq? @_name "react")))
+ arguments: ((template_string) @injection.content
+   (#offset! @injection.content 0 1 0 -1)
+   (#set! injection.language "tsx")))
+
+
+(call_expression
  function: (member_expression
    object: (identifier) @_name
      (#eq? @_name "svelte"))
@@ -48,3 +94,11 @@
    (#offset! @injection.content 0 1 0 -1)
    (#set! injection.language "svelte")))
 
+(call_expression
+ function: (call_expression
+   function: (member_expression
+    object: (identifier) @_name
+      (#eq? @_name "svelte")))
+ arguments: ((template_string) @injection.content
+   (#offset! @injection.content 0 1 0 -1)
+   (#set! injection.language "svelte")))
