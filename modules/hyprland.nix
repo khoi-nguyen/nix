efdf21363eaf94215ca69ed64e4781630bc1d20b
Khoi Nguyen <khoi@nguyen.me.uk>
2025-09-12 07:28:21 +0200

    Basic waybar config

--- modules/hyprland.nix
+++ modules/hyprland.nix
@@ -6,8 +6,33 @@
     xwayland.enable = true;
   };
 
+  home-manager.users.khoi = {
+    programs = {
+      waybar = {
+        enable = true;
+        settings = [
+          {
+            modules-left = [
+              "hyprland/workspaces"
+            ];
+            modules-center = [
+              "hyprland/window"
+            ];
+            modules-right = [
+              "pulseaudio"
+              "network"
+              "cpu"
+              "memory"
+              "battery"
+              "clock"
+            ];
+          }
+        ];
+      };
+    };
+  };
+
   environment.systemPackages = with pkgs; [
-    waybar
     wofi
   ];
 }
