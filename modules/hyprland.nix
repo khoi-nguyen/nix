10931057e7e7e26fa206848b404d32b9527e988f
Khoi Nguyen <khoi@nguyen.me.uk>
2025-09-12 09:03:14 +0200

    Basic hyprland config

--- modules/hyprland.nix
+++ modules/hyprland.nix
@@ -7,6 +7,61 @@
   };
 
   home-manager.users.khoi = {
+    wayland.windowManager.hyprland = {
+      enable = true;
+      package = null;
+      portalPackage = null;
+      settings = {
+        "$mod" = "SUPER";
+        exec-once = "waybar";
+        input = {
+          kb_layout = "us";
+          kb_variant = "dvorak-alt-intl";
+        };
+        bind = [
+          "$mod, Return, exec, kitty"
+          "$mod, B, exec, google-chrome-stable"
+          "$mod, D, killactive"
+          "$mod SHIFT, Q, exit"
+          "$mod, F, fullscreen"
+          "$mod, R, exec, wofi --show drun"
+          "$mod, h, movefocus, l"
+          "$mod, j, movefocus, d"
+          "$mod, k, movefocus, u"
+          "$mod, l, movefocus, r"
+          "$mod SHIFT, h, movewindow, l"
+          "$mod SHIFT, j, movewindow, d"
+          "$mod SHIFT, k, movewindow, u"
+          "$mod SHIFT, l, movewindow, r"
+        ]
+        ++ (builtins.concatLists (
+          builtins.genList (
+            i:
+            let
+              ws = i + 1;
+            in
+            [
+              "$mod, code:1${toString i}, workspace, ${toString ws}"
+              "$mod SHIFT, code:1${toString i}, movetoworkspace, ${toString ws}"
+            ]
+          ) 9
+        ));
+        monitor = "eDP-1, preferred, 0x0, 1";
+        general = {
+          gaps_out = 5;
+          border_size = 2;
+          "col.active_border" = "rgba(33ccffee) rgba(00ff99ee) 45deg";
+          "col.inactive_border" = "rgba(595959aa)";
+        };
+        decoration = {
+          rounding = 10;
+        };
+        misc = {
+          force_default_wallpaper = -1;
+          disable_hyprland_logo = true;
+        };
+      };
+    };
     programs = {
       waybar = {
         enable = true;
